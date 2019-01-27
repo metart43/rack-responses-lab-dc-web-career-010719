3165bea6b990cbf32e31ee1661b889493a39c9ac
github username <metelskyi88@gmail.com>
2019-01-26 22:34:02 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,17 @@
+require 'pry'
+
+class Application
+
+def call(env)
+  resp = Rack::Response.new
+  time = Time.now
+  binding.pry
+  if time.hour <= 12
+    resp.write("Morning")
+  else
+    resp.write("Afternoon")
+  end
+  resp.finish
+end
+
+end
